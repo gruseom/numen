c0e7ecf24e14f04a80ff4f0de4d4f3127a29cdd3
L-11 <lelfjior@gmail.com>
2016-08-24 21:21:46 -0500

    Minor fix

--- numen.js
+++ numen.js
@@ -2,6 +2,9 @@ var vm = require('vm');
 var fs = require('fs');
 var net = require('net');
 
+var reader = require('reader');
+var compiler = require('compiler');
+
 global.D = v8debug.Debug; // the V8 debugger
 var S = null; // server
 var C = null; // client connection
@@ -22,7 +25,7 @@ function response (req) {
     } else if (req.load) {
         res = { 'loaded' : numenLoad(req.load, req.breakpoints) };
     } else if (req.compile) {
-        res = { 'compiled' : compile(expand(read_string(req.compile))) };
+        res = { 'compiled' : compiler.compile(compiler.expand(reader["read-string"](req.compile))) };
     } else if (req.source) {
         res = { 'script' : req.source, 'source' : source(req.source) };
     } else if (req.breakpoint) {
@@ -96,7 +99,7 @@ function evaluateAtTopLevel (code, breakAtStart) {
         D.breakExecution();
     }
     if (runningLumen) {
-        return clientValue(eval(read_string(code)));
+        return clientValue(compiler.eval(reader["read-string"](code)));
     } else {
         return clientValue(vm.runInThisContext(code, uniqueEvalScriptName()));
     }
